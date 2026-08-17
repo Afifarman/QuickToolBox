1170bd3537d2f2720ec7c3b28f19ce15166dadff
Arif Arman Siuam <97172364+Afifarman@users.noreply.github.com>
2026-08-17 08:46:35 +0600

    Refactor RootLayout by removing HTML tags
    Removed unnecessary HTML structure from RootLayout.

--- app/layout.js
+++ app/layout.js
@@ -7,15 +7,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4690893675414003"
-          crossOrigin="anonymous"
-        ></script>
-      </head>
-      <body>{children}</body>
-    </html>
+    
+      
+        
+      
+      {children}
+    
   );
 }
